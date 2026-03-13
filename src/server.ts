3e78e4c8ff2051652ee0a88fb2d80e00eb9f1a24
ARGHYA <samantaarghya351@gmail.com>
2026-03-13 21:45:48 +0530

    0.0.0.0

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,8 @@ import app from './app';
 import { env } from './config/env';
 import { logger } from './utils/logger';
 
-const port = env.PORT;
+const port = Number(env.PORT);
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     logger.info(`Server is running on port ${port}`);
 });
