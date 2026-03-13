d829c6b49d4ac213c0ca4bb90355aa637aee7c75
ARGHYA <samantaarghya351@gmail.com>
2026-03-13 21:11:46 +0530

    Added the frontend url

--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,10 @@ const app = express();
 
 // Middlewares
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    credentials: true,
+}));
 app.use(express.json());
 app.use(morgan('dev'));
 
