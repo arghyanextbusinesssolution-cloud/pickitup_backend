1d029efed5b414b19bbc4f0ef3a4c201cabaa651
ARGHYA <samantaarghya351@gmail.com>
2026-05-15 16:22:26 +0530

    Stop tracking dist folder and use Render build

--- dist/config/db.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const client_1 = require("@prisma/client");
-const env_1 = require("./env");
-const prisma = new client_1.PrismaClient({
-    datasourceUrl: env_1.env.DATABASE_URL,
-});
-exports.default = prisma;
